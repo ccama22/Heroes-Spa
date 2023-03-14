9991808603d918b8a558fb4f490b2308d0db728a
ccama22 <ccamacruz2018@gmail.com>
2023-03-13 23:13:03 -0500

    practica code

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 
 export const MarvelPage = () => {
   // const location = useLocation();
-  console.log("dsd");
+  console.log("dsdf");
   return (
     <>
       <h1>MarvelPage</h1>
