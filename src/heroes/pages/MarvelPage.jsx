d3962ea008fb44dbae653de855abc79fb2fcf115
ccama22 <ccamacruz2018@gmail.com>
2023-03-13 21:37:08 -0500

    practica code final 9

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 
 export const MarvelPage = () => {
   // const location = useLocation();
-  console.log("d");
+  console.log("ds");
   return (
     <>
       <h1>MarvelPage</h1>
